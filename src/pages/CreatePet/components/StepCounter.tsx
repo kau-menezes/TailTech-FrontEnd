ab6090c5e28c601c74f9ad41d77df061174edae1
MuryloSaladino <murylosaladino@gmail.com>
2024-10-25 09:06:56 -0300

    fix key

--- src/pages/CreatePet/components/StepCounter.tsx
+++ src/pages/CreatePet/components/StepCounter.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react/jsx-runtime";
 import { v4 as uuid } from "uuid";
 
 interface IStepCounterProps {
@@ -9,16 +10,16 @@ export default function StepCounter({ currentStep, steps }:IStepCounterProps) {
     return(
         <div className="w-full flex items-center relative gap-1">
             {steps.map((step, index) => (
-                <>
-                    <div key={uuid()} className={`flex justify-center items-center rounded-full w-44 p-2 box-border aspect-square ${index+1 <= currentStep ? "bg-mainPurple" : "bg-gray-400"}`}>
+                <Fragment key={uuid()}>
+                    <div className={`flex justify-center items-center rounded-full w-44 p-2 box-border aspect-square ${index+1 <= currentStep ? "bg-mainPurple" : "bg-gray-400"}`}>
                         <span className="text-4xl text-white font-semibold">{ index+1 }.</span>
                         <h5 className={`absolute right-0 left-0 top-24 m-auto w-full text-center text-mainPurple text-2xl font-semibold ${index+1 != currentStep && "hidden"}`}>{step}</h5>
                     </div>
                     {
                         index+1 < steps.length && 
-                        <div key={uuid()} className={`h-1 w-full rounded-lg ${index+1 < currentStep ? "bg-mainPurple" : "bg-gray-400"}`}></div>
+                        <div className={`h-1 w-full rounded-lg ${index+1 < currentStep ? "bg-mainPurple" : "bg-gray-400"}`}></div>
                     }
-                </>
+                </Fragment>
             ))}
         </div>
     )
